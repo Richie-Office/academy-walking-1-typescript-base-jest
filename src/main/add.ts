274260e21b621d3d0e497b57fee4b547b77e6789
uligruener-cn <uli.gruener@cluno.com>
2021-06-15 11:13:03 +0200

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/add.ts
+++ src/main/add.ts
@@ -1,11 +1,11 @@
 export function Add(input: string): number {
-    if (input === undefined || input.length === 0) {
-        return 0;
-    }
-  if (input.includes(',')) {
-      let arr = input.split(',');
-      return Add(arr[0]) + Add(arr[1]) + Add(arr[2]) ;
+  if (input.length === 0) {
+    return 0;
   }
 
-  return parseInt(input);
+  let sum = 0;
+  input.split(",").forEach((part) => {
+    sum += parseInt(part);
+  });
+  return sum;
 }
--- src/test/add.test.ts
+++ src/test/add.test.ts
@@ -1,16 +1,34 @@
 import { Add } from "../main/add";
 
-describe('Addition', () => {
-    it("should receive an empty string as input and return 0", () => {
-        let input = ""
-        expect(Add(input)).toBe(0);
-    })
+describe("Addition", () => {
+  it("should receive an empty string as input and return 0", () => {
+    let input = "";
+    expect(Add(input)).toBe(0);
+  });
 
-    it.each([["1",1], ["2",2], ["3",3], ["4",4], ['19', 19],["1,2", 3], ["100,66", 166], ["200,1", 201]])("Should receive %s and return %i", (input, output)=>{
-        expect(Add(input)).toBe(output);
-    })
+  it.each([
+    ["1", 1],
+    ["2", 2],
+    ["3", 3],
+    ["4", 4],
+    ["19", 19],
+    ["1,2", 3],
+    ["100,66", 166],
+    ["200,1", 201],
+  ])("Should receive %s and return %i", (input, output) => {
+    expect(Add(input)).toBe(output);
+  });
 
-    it.each([["100,200,300", 600], ["100,100,100,100", 400]])("Should receive %s and return %i", (input, output)=>{
-        expect(Add(input)).toBe(output);
-    })
-})
+  it.each([
+    ["100,200,300", 600],
+    ["100,100,100,100", 400],
+    ["100,100,100,100,1,1,1,1", 404],
+  ])("Should receive %s and return %i", (input, output) => {
+    expect(Add(input)).toBe(output);
+  });
+
+  it("Should return 6 for input '1\n2,3'", () => {
+    let input = "1\n2,3";
+      expect(Add(input)).toBe(6);
+  })
+});
