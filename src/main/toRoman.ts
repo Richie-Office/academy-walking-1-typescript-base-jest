54963f403d653ee949901c1f6bee8b67409c1cba
Hudson Steffani Soares Rosa <hudsonssrosa@gmail.com>
2021-06-29 11:25:19 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/toRoman.ts
+++ src/main/toRoman.ts
@@ -1,6 +1,6 @@
 
 export function toRoman(input: number): string  {
-  let roman = [ "I", "II", "III", "IV", "V", "VI", "VII" ]
+  let roman = [ "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X" ]
 
   return roman[input-1]
 }
--- src/test/toRoman.test.ts
+++ src/test/toRoman.test.ts
@@ -2,7 +2,19 @@ import { idText } from 'typescript';
 import { toRoman } from "../main/toRoman";
 
 describe("To Roman", () => {
-  test.each([[1,"I"],[2,"II"],[3,"III"],[4,"IV"],[5,"V"],[6,"VI"],[7,"VII"]])("should receive a number %i to be returned as roman numeral %s", (num: any, roman: any) => {
+  it.each([
+    [1,"I"],
+    [2,"II"],
+    [3,"III"],
+    [4,"IV"],
+    [5,"V"],
+    [6,"VI"],
+    [7,"VII"],
+    [8,"VIII"],
+    [9,"IX"],
+    [10,"X"]
+  ])
+  ("should receive a number %i to be returned as Roman numeral %s", (num: any, roman: any) => {
     expect(toRoman(num)).toBe(roman);
   });
 });
