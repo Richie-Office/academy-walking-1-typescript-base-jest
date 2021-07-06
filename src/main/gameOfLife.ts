43711fc2e5f55b3213d070818248d67da47177f0
uligruener-cn <uli.gruener@cluno.com>
2021-07-06 12:32:08 +0200

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/gameOfLife.ts
+++ src/main/gameOfLife.ts
@@ -1,6 +1,7 @@
+export class GameOfLife {
+  private grid;
 
-export function toRoman(input: number): string  {
-  let roman = [ "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X" ]
-
-  return roman[input-1]
+  constructor(initialGrid: number[][]) {
+    this.grid = initialGrid;
+  }
 }
--- src/test/gameOfLife.test.ts
+++ src/test/gameOfLife.test.ts
@@ -1,6 +1,21 @@
-import { idText } from 'typescript';
-import { toRoman } from "../main/gameOfLife";
+import { idText } from "typescript";
+import { GameOfLife } from "../main/gameOfLife";
 
-describe("To Roman", () => {
-  
+describe("GameOfLife test", () => {
+  test("Should do an iteration", () => {
+    const gameOfLife = new GameOfLife([
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+    ]);
+    expect(gameOfLife.print()).toBe([
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+    ]);
+  });
 });
